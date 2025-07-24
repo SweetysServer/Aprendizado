a9c5bb1d2edceaad6d587863bfdac6c3da9275ba
SweetysServer <lgvalentinereis@gmail.com>
2025-07-24 13:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h1 = document.QuerySelector("h1");
-const button = document.QuerySelector("button")
+const h1 = document.querySelector("h1");
+const button = document.querySelector("button")
 
 button.addEventListener("click", function() {
   h1.textContent = "Quem é esse atrás de você?";
