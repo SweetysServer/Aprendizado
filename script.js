247a3641442b6ee889462965490607bffff42ff3
SweetysServer <lgvalentinereis@gmail.com>
2025-07-24 12:58:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const h1 = document.QuerySelector("h1");
+const button = document.QuerySelector("button")
+
+button.addEventListener("click", function() {
+  h1.textContent = "Quem é esse atrás de você?";
+});
+  
+                        
